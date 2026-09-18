31d12eba239ca2bd55916e3aba12643d3fb82230
Anika Mukka <anika.mukka@gmail.com>
2026-09-18 16:39:46 +0000

    add About page

--- server.js
+++ server.js
@@ -1,40 +1,17 @@
 import express from 'express';
 
 const app = express();
+app.set("view engine", "ejs");
 const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, web!');
 });
 
-app.get('/about', (req, res) => {
-  res.send('Happy FDOC!!!');
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
 });
 
-const projects = [
-  { name: 'Weather app', tag: 'javascript' },
-  { name: 'Portfolio site', tag: 'express' },
-  { name: 'Budget tracker', tag: 'python' },
-];
-
-app.get('/projects', (req, res)  => {
-  const tag = req.query.tag;
-  // filter `projects` here, based on your decision above
-  if (!tag) {
-    return res.send(projects)
-  }
-
-    const filteredProjects = projects.filter(project => project.tag == tag);
-
-    if (filteredProjects.length == 0){
-      return res.send("No tags match this search")
-    }
-    res.send(filteredProjects)
-
-}); 
-
-
-
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
